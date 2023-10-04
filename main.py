6f79652c97cc24c36e685d624e82875df019aaf2
Sebastian <sebastiandamonbreuer@gmail.com>
2023-10-04 22:43:10 +0200

    test

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import requests
 
 BASE_URL = 'https://httpbin.org'
 
+# data = {'data1': 'Hello', 'data2': 'world'}
+# response = requests.post(BASE_URL + '/post', data=data)
+#
+# if response.status_code == 200:
+#     print("Post request was successful")
+# else:
+#     print("failed")
 
-response = requests.get(BASE_URL + '/get')
-response = response.json()
+username = 'testuser'
+password = 'testpass'
 
+response = requests.get(BASE_URL + f'/basic-auth/{username}/{password}', auth=(username, password))
 
-del response["origin"]
-
-print(response)
+if response.status_code == 200:
+    print("GET request was successful")
+else:
+    print("failed")
